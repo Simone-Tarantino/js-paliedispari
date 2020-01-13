e3eb0d4f11027ce827e50b09d081e0a2315d386f
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-01-13 14:39:58 +0100

    Palindroma Funziona

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,8 +1,21 @@
 // Scrivi una funzione per capire se la parola è palindroma
 
 var wordArray = [];
+var wordArrayReverse =[];
 var word = prompt("parola");
+var pali = false;
 for (var i = 0; i < word.length; i++) {
   var digit = wordArray.push(word[i]);
+  var digitReverse = wordArrayReverse.push(word[i]);
 }
 console.log(wordArray);
+wordArrayReverse.reverse();
+console.log(wordArrayReverse);
+for (var j = 0; j < wordArray.length; j++) {
+  if (wordArray[j] == wordArrayReverse[j]) {
+    pali = true;
+  } else {
+    pali = false;
+  }
+}
+console.log(pali);
