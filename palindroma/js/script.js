27987156540ccb7e324bcb5135dd4b575ce18b40
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-01-13 14:05:46 +0100

    Palindroma WIP#2

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,18 +1,5 @@
 // Scrivi una funzione per capire se la parola è palindroma
 
-// var word = prompt("parola");
-// var palindroma = false;
-// for (var i = 0; i < Math.ceil(word.length/2) && palindroma == true; i++ ) {
-//   if (word[i] == word[-i]) {
-//     palindroma = true;
-//     console.log(palindroma);
-//   } else {
-//     palindroma = false;
-//     console.log(palindroma);
-//   }
-// }
-// console.log(palindroma);
-
 var wordArray = [];
 var word = prompt("parola");
 for (var i = 0; i < word.length; i++) {
