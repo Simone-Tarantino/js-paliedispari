e3b71a04dc4891192063bb7b8147d2c269fbc85e
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-01-13 15:26:46 +0100

    Esercizio completo

--- pari/js/script.js
+++ pari/js/script.js
@@ -5,7 +5,33 @@
 // Sommiamo i due numeri e dichiariamo chi ha
 // vinto.
 
-// var userOddEven = prompt("Pari o dispari?");
+function evenOrOdd(numero) {
+  if (numero % 2 == 0) {
+    risultato = "pari";
+    return risultato;
+  } else {
+    risultato = "dispari";
+    return risultato;
+  }
+}
+
+function winOrLose(valore1, valore2) {
+  if (valore1 == valore2) {
+    return "Hai vinto";
+  } else {
+    return "Hai perso";
+  }
+}
+
+var userEvenOdd = prompt("Pari o dispari?");
+console.log("Hai scommesso che il numero è " + userEvenOdd);
 var userNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
+console.log("Il numero che hai scelto è il " + userNumber);
 var randomCPUNumber = Math.floor((Math.random() * 5) + 1);
+console.log("Il numero del computer è " + randomCPUNumber);
 var totalNumber = userNumber + randomCPUNumber;
+console.log("Il numero totale è " + totalNumber);
+var evenOddResult = evenOrOdd(totalNumber);
+console.log("Il numero totale è " + evenOddResult);
+var gameResult = winOrLose(userEvenOdd, evenOddResult);
+console.log(gameResult);
